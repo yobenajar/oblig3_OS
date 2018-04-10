652c299b3dd0c96a4355bef838b205c2d4c0c0a9
Yobe Najar <yoben@stud.ntnu.no>
2018-04-10 19:17:30 +0200

    Endringer

--- procmi.ps1
+++ procmi.ps1
@@ -3,10 +3,9 @@ $date = Get-Date -UFormat '%y%m%d-%H%M%S'
 
 for ($i=0; $i -lt $args.Count; $i++) {
 
-		$filnavn = ""+$args[$i]+"--"+$date+".txt"
+		#$filnavn = ""+$args[$i] + "--" + $date + ".meminfo"
 
-		#"$args[$i]-{0:yyyyMMdd-HHmmss}.meminfo" -f (Get-Date)
-
+		$filnavn = ""+ $args[$i] + "-- -{0:yyyyMMdd-HHmmss}.meminfo" -f (Get-Date)
 		$minne = (Get-Process -id $args[$i]).VirtualMemorySize / 1MB
 		$workingSet = (Get-Process -id $args[$i]).WorkingSet
 
