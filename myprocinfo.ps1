d95ea685bb984aebc8201ac91d1c94ee4f897cf4
Yobe Najar <yoben@stud.ntnu.no>
2018-04-10 17:26:25 +0200

    Endringer

--- myprocinfo.ps1
+++ myprocinfo.ps1
@@ -1,46 +1,46 @@
 function meny {
-	Write-host
-	Write-Host "1 - Hvem er jeg og hva er navnet paa dette scriptet?"
-	Write-host "2 - Hvor lenge er det siden siste boot?"
-	Write-host "3 - Hvor mange prosesser og tr ̊ader finnes?"
-	Write-host "4 - Hvor mange context switch'er fant sted siste sekund?"
-	Write-host "5 - Hvor stor andel av CPU-tiden ble benyttet i kernelmode og i usermode siste sekund?"
-	Write-host "6 - Hvor mange interrupts fant sted siste sekund?"
-	Write-host "9 - Avslutt dette scriptet"
+	Write-Output "
+	 1 - Hvem er jeg og hva er navnet paa dette scriptet?
+	 2 - Hvor lenge er det siden siste boot?
+	 3 - Hvor mange prosesser og tr ̊ader finnes?
+	 4 - Hvor mange context switch'er fant sted siste sekund?
+	 5 - Hvor stor andel av CPU-tiden ble benyttet i kernelmode og i usermode siste sekund?
+	 6 - Hvor mange interrupts fant sted siste sekund?
+	 9 - Avslutt dette scriptet"
 }
 
 meny
 
 while ($ans -ne 9) {
-	Write-Host "Skriv inn et tall: "
+	Write-Output "Skriv inn et tall: "
 	$ans=Read-Host
 
 
 	switch ($ans) {
-		1 { Write-host "Jeg er $(whoami), og scriptet heter $($MyInvocation.MyCommand.Name)" }
+		1 { Write-Output "Jeg er $(whoami), og scriptet heter $($MyInvocation.MyCommand.Name)" }
 
 		2 {
-			#Write-Host "Tid siden siste boot: $(Get-Uptime)"
+			#Write-Output "Tid siden siste boot: $(Get-Uptime)"
 
 			$uptime = (Get-Date) - (Get-CimInstance -ClassName win32_operatingsystem).LastBootUpTime
-            Write-Host("Siste boot: " + $uptime.Hours + "h, " + $uptime.Minutes + "m")
+            Write-Output("Siste boot: " + $uptime.Hours + "h, " + $uptime.Minutes + "m")
 		}
 
 		3 {
 			#$count = 0
 			#Get-Process | ForEach-Object {$count = $([int]$count + $_.Threads.Count)}
-			#Write-Host "Antall prosesser: $([int]@(Get-Process).Count)"
-			#Write-Host "Antall tråder: $($count)"
+			#Write-Output "Antall prosesser: $([int]@(Get-Process).Count)"
+			#Write-Output "Antall tråder: $($count)"
 
 			$trader = (Get-CimInstance -ClassName win32_Thread | Get-Unique | Measure-Object).Count
             $prosesser = (Get-Process | Sort-Object name| Get-Unique | Measure-Object).count
             Write-Output ("Det finnes" + $trader + "tråder")
-            Write-Host "Det finnes $prosesser prosesser"
+            Write-Output "Det finnes $prosesser prosesser"
 		}
 
 		4 {
 			$contextSwitch = (Get-CimInstance -ClassName Win32_PerfFormattedData_PerfOS_System).ContextSwitchesPersec
-			Write-Host "Antall context switch i det siste sekundet: $contextSwitch"
+			Write-Output "Antall context switch i det siste sekundet: $contextSwitch"
 		}
 
 		5 {
@@ -64,16 +64,16 @@ while ($ans -ne 9) {
 
 			$prosent = 100 / $sum
 
-			Write-Output($user * $prosent + "% av det siste sekundet til usermode")
-			Write-host "$kernel * $prosent % av det siste sekundet til kernelmode"
+			Write-Output ($user * $prosent + "% av det siste sekundet til usermode")
+			Write-Output ($kernel * $prosent + "% av det siste sekundet til kernelmode")
 		}
 
 		6 {
 			$interrupts = (Get-CimInstance -ClassName Win32_PerfFormattedData_Counters_ProcessorInformation).InterruptsPersec
-			Write-Host "Antall interrups i det siste sekundet: $interrupts"
+			Write-Output "Antall interrups i det siste sekundet: $interrupts"
 		}
 
-		9 { Write-Host "Programmet er avsluttet" }
+		9 { Write-Output "Programmet er avsluttet" }
 
 	}
 }
