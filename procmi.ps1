6adf0d6998e202e40906d478766ffb7b428208c6
Yobe Najar <yoben@stud.ntnu.no>
2018-04-10 19:58:06 +0200

    Siste

--- procmi.ps1
+++ procmi.ps1
@@ -2,11 +2,11 @@
 
 for ($i=0; $i -lt $args.Count; $i++) {
 
-		$filnavn = "" + $args[$i] + "--{0:yyyyMMdd--HHmmss}.meminfo" -f (Get-Date)
-		$minne = (Get-Process -id $args[$i]).VirtualMemorySize / 1MB
-		$workingSet = (Get-Process -id $args[$i]).WorkingSet
+	$filnavn = "" + $args[$i] + "--{0:yyyyMMdd--HHmmss}.meminfo" -f (Get-Date)
+	$minne = (Get-Process -id $args[$i]).VirtualMemorySize / 1MB
+	$workingSet = (Get-Process -id $args[$i]).WorkingSet
 
-		New-Item -Path . -Itemtype File -Name $filnavn -Force
-		Add-Content -Path $filnavn -Value ("********* Minne info om prosess med PID " + $args[$i] + " *********" +
-		"`nTotal bruk av virtuelt minne: " + $minne + " MB" + "`nStorrelse paa Working Set: " + $workingSet + " KB" )
+	New-Item -Path . -Itemtype File -Name $filnavn -Force
+	Add-Content -Path $filnavn -Value ("********* Minne info om prosess med PID " + $args[$i] + " *********" +
+	"`nTotal bruk av virtuelt minne: " + $minne + " MB" + "`nStorrelse paa Working Set: " + $workingSet + " KB" )
 }
