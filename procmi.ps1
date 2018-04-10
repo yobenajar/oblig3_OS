cca435de57a2db10d8ff84a995643f2dc37bdb2b
Yobe Najar <yoben@stud.ntnu.no>
2018-04-10 19:42:28 +0200

    Endringer

--- procmi.ps1
+++ procmi.ps1
@@ -7,7 +7,7 @@ for ($i=0; $i -lt $args.Count; $i++) {
 		$workingSet = (Get-Process -id $args[$i]).WorkingSet
 
 		New-Item -Path . -Itemtype File -Name $filnavn -Force
-		Add-Content -Path $filnavn -Value ("
-		********* Minne info om prosess med PID" + $args[$i] + "*********" +
+		Add-Content -Path $filnavn -Value
+		("********* Minne info om prosess med PID" + $args[$i] + "*********" +
 		"`nTotal bruk av virtuelt minne: " + $minne + "MB" + "`nStorrelse paa Working Set: " + $workingSet + "KB" )
 }
