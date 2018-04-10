283d3c497bd4c3a74d1b5b36bca13227e44aa7fa
Yobe Najar <yoben@stud.ntnu.no>
2018-04-10 19:30:23 +0200

    Endringer

--- procmi.ps1
+++ procmi.ps1
@@ -1,15 +1,15 @@
 
 
-for ($i=0; $i -lt $args.Count; $i++) {
+for ($i=0; $i -lt $pid.Count; $i++) {
 
-		$filnavn = ""+ $args[$i] + "--{0:yyyyMMdd--HHmmss}.meminfo" -f (Get-Date)
-		$minne = (Get-Process -id $args[$i]).VirtualMemorySize / 1MB
-		$workingSet = (Get-Process -id $args[$i]).WorkingSet
+		$filnavn = ""+ $pid[$i] + "--{0:yyyyMMdd--HHmmss}.meminfo" -f (Get-Date)
+		$minne = (Get-Process -id $pid[$i]).VirtualMemorySize / 1MB
+		$workingSet = (Get-Process -id $pid[$i]).WorkingSet
 
 		New-Item -Path . -Itemtype File -Name $filnavn -Force
 		Add-Content -Path $filnavn -Value ("
-		Minne info om prosess med PID " + $args[$i] +
-		"`nTotal bruk av virtuelt minne: " + $minne +
+		********* Minne info om prosess med PID ********* " + $pid[$i] +
+		"`nTotal bruk av virtuelt minne: " + $minne + "MB" +
 		"`nStorrelse paa Working Set: " + $workingSet
-		)
+		+ "KB")
 }
