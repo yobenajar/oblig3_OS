ec460b124d487e38db14b9faecd2c24f55d5df05
Yobe Najar <yoben@stud.ntnu.no>
2018-04-10 18:42:19 +0200

    Endringer

--- procmi.ps1
+++ procmi.ps1
@@ -6,8 +6,9 @@ for ($i=0; $i -lt $args.Count; $i++) {
 		#$minne = (Get-Process -id $args[$i]).VirtualMemorySize / 1MB
 		#$workingSet = (Get-Process -id $args[$i]).WorkingSet
 
+		New-item Path . -Itemype File -Name $filnavn -Force
 	{
-		Write-host "************** Minne info om prosess med PID" + $args[$i] + "**************"
+		Write-Output("************** Minne info om prosess med PID" + $args[$i] + "**************")
 		Write-Host "Total bruk av virtuelt minne: $minne"
 		Write-Host "Størrelse på Working Set: $workingset"
 
